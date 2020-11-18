2fc8a84e80d2487436044850a598e17a3e4985cf
giorgi kutateladze <giorg.kutateladze.1@btu.edu.ge>
2020-11-18 18:29:48 +0400

    Tic-Tac-Toe

--- app/src/main/java/com/example/tic_tac_toe/MainActivity.kt
+++ app/src/main/java/com/example/tic_tac_toe/MainActivity.kt
@@ -86,23 +86,24 @@ class MainActivity : AppCompatActivity() {
 
     private fun checkWinner() {
         if (Button00.text.isNotEmpty() && Button00.text.toString() == Button01.text.toString() && Button00.text.toString() == Button02.text.toString()) {
-            Toast.makeText(this, "Winner ${Button00.text}", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Winner is ${Button00.text}", Toast.LENGTH_SHORT).show()
         } else if (Button10.text.isNotEmpty() && Button10.text.toString() == Button11.text.toString() && Button10.text.toString() == Button12.text.toString()) {
-            Toast.makeText(this, "Winner ${Button10.text}", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Winner is ${Button10.text}", Toast.LENGTH_SHORT).show()
         } else if (Button20.text.isNotEmpty() && Button20.text.toString() == Button21.text.toString() && Button20.text.toString() == Button22.text.toString()) {
-            Toast.makeText(this, "Winner ${Button20.text}", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Winner is ${Button20.text}", Toast.LENGTH_SHORT).show()
         } else if (Button00.text.isNotEmpty() && Button00.text.toString() == Button10.text.toString() && Button00.text.toString() == Button20.text.toString()) {
-            Toast.makeText(this, "Winner ${Button00.text}", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Winner is ${Button00.text}", Toast.LENGTH_SHORT).show()
         } else if (Button01.text.isNotEmpty() && Button01.text.toString() == Button11.text.toString() && Button01.text.toString() == Button21.text.toString()) {
-            Toast.makeText(this, "Winner ${Button01.text}", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Winner is ${Button01.text}", Toast.LENGTH_SHORT).show()
         } else if (Button02.text.isNotEmpty() && Button02.text.toString() == Button12.text.toString() && Button02.text.toString() == Button22.text.toString()) {
-            Toast.makeText(this, "Winner ${Button02.text}", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Winner is ${Button02.text}", Toast.LENGTH_SHORT).show()
         } else if (Button00.text.isNotEmpty() && Button00.text.toString() == Button11.text.toString() && Button00.text.toString() == Button22.text.toString()) {
-            Toast.makeText(this, "Winner ${Button00.text}", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Winner is ${Button00.text}", Toast.LENGTH_SHORT).show()
         } else if (Button02.text.isNotEmpty() && Button02.text.toString() == Button11.text.toString() && Button02.text.toString() == Button20.text.toString()) {
-            Toast.makeText(this, "Winner ${Button02.text}", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Winner is ${Button02.text}", Toast.LENGTH_SHORT).show()
 
-        } else if (Button00.text.isNotEmpty() && Button01.text.isNotEmpty() && Button02.text.isNotEmpty() && Button10.text.isNotEmpty() && Button11.text.isNotEmpty() && Button12.text.isNotEmpty() && Button20.text.isNotEmpty() && Button21.text.isNotEmpty() && Button22.text.isNotEmpty()) {
+        } else if (Button00.text.isNotEmpty() && Button01.text.isNotEmpty() && Button02.text.isNotEmpty() && Button10.text.isNotEmpty() && Button11.text.isNotEmpty()
+            && Button12.text.isNotEmpty() && Button20.text.isNotEmpty() && Button21.text.isNotEmpty() && Button22.text.isNotEmpty()) {
             Toast.makeText(this, "Draw", Toast.LENGTH_SHORT).show()
         }
 
