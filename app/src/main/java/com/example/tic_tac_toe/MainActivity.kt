eeaaef7103af8668a997908a6c90f060eeccbbd2
giorgi kutateladze <giorg.kutateladze.1@btu.edu.ge>
2020-11-18 18:05:31 +0400

    Tic-Tac-Toe

--- app/src/main/java/com/example/tic_tac_toe/MainActivity.kt
+++ app/src/main/java/com/example/tic_tac_toe/MainActivity.kt
@@ -45,7 +45,26 @@ class MainActivity : AppCompatActivity() {
         Button22.setOnClickListener {
             changeButton(Button22)
         }
-        playAgainButton.setOnClickListener {
+        gameRestartButton.setOnClickListener {
+            isPlayerOne = true
+            Button00.isClickable = true
+            Button00.text = ""
+            Button01.isClickable = true
+            Button01.text = ""
+            Button02.isClickable = true
+            Button02.text = ""
+            Button10.isClickable = true
+            Button10.text = ""
+            Button11.isClickable = true
+            Button11.text = ""
+            Button12.isClickable = true
+            Button12.text = ""
+            Button20.isClickable = true
+            Button20.text = ""
+            Button21.isClickable = true
+            Button21.text = ""
+            Button22.isClickable = true
+            Button22.text = ""
 
         }
 
@@ -83,9 +102,9 @@ class MainActivity : AppCompatActivity() {
         } else if (Button02.text.isNotEmpty() && Button02.text.toString() == Button11.text.toString() && Button02.text.toString() == Button20.text.toString()) {
             Toast.makeText(this, "Winner ${Button02.text}", Toast.LENGTH_SHORT).show()
 
+        } else if (Button00.text.isNotEmpty() && Button01.text.isNotEmpty() && Button02.text.isNotEmpty() && Button10.text.isNotEmpty() && Button11.text.isNotEmpty() && Button12.text.isNotEmpty() && Button20.text.isNotEmpty() && Button21.text.isNotEmpty() && Button22.text.isNotEmpty()) {
+            Toast.makeText(this, "Draw", Toast.LENGTH_SHORT).show()
         }
-         else if (Button00.text.isNotEmpty() && Button01.text.isNotEmpty() && Button02.text.isNotEmpty() && Button10.text.isNotEmpty() && Button11.text.isNotEmpty() && Button12.text.isNotEmpty() && Button20.text.isNotEmpty() && Button21.text.isNotEmpty() && Button22.text.isNotEmpty()){
-          Toast.makeText(this, "Draw", Toast.LENGTH_SHORT).show()}
 
     }
 }
